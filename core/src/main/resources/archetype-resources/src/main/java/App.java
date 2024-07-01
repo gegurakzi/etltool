62b4ec5b5da5ed3903b0a68bec69e78cf5413d9e
Taehee Lee <prussian1933@naver.com>
2024-07-01 13:11:50 +0900

    initial commit

---
+++ core/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $io.malachai.etltool;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
